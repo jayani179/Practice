5760d53a4554cfe10c440f4865d441df58feaca5
Jayani Paropkari <jayani.shah@gmail.com>
2023-02-20 16:28:13 +0000

    Practice

---
+++ Java/JavaTopics.java
@@ -0,0 +1,16 @@
+package Java;
+//name changed to java from javatopics
+public class JavaTopics {
+    /*
+    name changed from java to javatopics
+     */
+    public static void main(String[] args) {
+
+
+        System.out.println("1.What is JAVA");
+        System.out.println("2.Package and Class");
+        System.out.println("3.Import statement");
+        System.out.println("4.Main class");
+        System.out.println("5.Access Modifiers");
+    }
+}
---
+++ manual/ManualTopics.java
@@ -0,0 +1,22 @@
+package manual;
+//name changed to manual from manualtopics
+
+public class ManualTopics {
+    /* refactor or rename
+       name changed from manual to manualtopics
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("1.Manual Testing");
+        System.out.println("2.Importance Of Manual testing");
+        System.out.println("3.Qualification of tester");
+        System.out.println("4.Importance of Testplan");
+        System.out.println("5.SDLC");
+        System.out.println("6.STLC");
+        System.out.println("7.RTM");
+        System.out.println("8.Testing Techniques");
+        System.out.println("9.Testing levels");
+        System.out.println("10.UAT Testing");
+    }
+}
